fadb5ca8e730d7c255b70339d0cc0fc020e1903b
Iancu <iancuaa@medads.uk-erlangen.de>
2025-08-01 16:24:26 +0200

    fix: add return statement if no laterality mapping found

--- dicom2fhir/dicom2fhirutils.py
+++ dicom2fhir/dicom2fhirutils.py
@@ -59,6 +59,7 @@ def get_lat_snomed(laterality: str, sctmapping: pd.DataFrame):
     if not match.empty:
         row = match.iloc[0]
         return row["Code Value"], row["Code Meaning"]
+    return None, None
 
 def _coding(d: dict) -> coding.Coding | None:
 
